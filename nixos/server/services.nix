2bc62982976a0cd581b5fc1ff2c760afe701ce1c
Linus Torvalds <me@theholytachanka.com>
2023-12-24 14:14:56 +0200

    Added Bazarr

--- nixos/server/services.nix
+++ nixos/server/services.nix
@@ -3,15 +3,32 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.oci-containers.containers.filebrowser = {
-    image = "docker://filebrowser/filebrowser";
-    volumes = [
-      "/portainer:/srv"
-      "/data/AppData/filebrowser/config/database.db:/database.db"
-    ];
-    ports = [ 
-      "1010:80" 
-    ];
-    autoStart = true;
+  virtualisation.oci-containers.containers = {
+    filebrowser = {
+      image = "docker://filebrowser/filebrowser";
+      volumes = [
+        "/portainer:/srv"
+        "/data/AppData/filebrowser/config/database.db:/database.db"
+      ];
+      ports = [ 
+        "1010:80" 
+      ];
+      autoStart = true;
+    };
+    bazarr = {
+      image = "docker://lscr.io/linuxserver/bazarr:1.4.0";
+      environment = [
+        "TZ=Europe/Sofia"
+      ]
+      volumes = [
+        "/data/AppData/Bazarr:/config"
+        "/portainer/Movies:/movies"
+        "/portainer/TV:/tv"
+      ];
+      ports = [ 
+        "6767:6767" 
+      ];
+      autoStart = true;
+    };
   };
 }
