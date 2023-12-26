bf7f9d4ae107301b942eb421784544fa64d3e1af
Linus Torvalds <me@theholytachanka.com>
2023-12-26 12:02:17 +0200

    fixed netdata

--- nixos/server/services.nix
+++ nixos/server/services.nix
@@ -14,19 +14,9 @@
   services.netdata = {
     enable = true;
 
-    config = {
-      global = {
-        # uncomment to reduce memory to 32 MB
-        #"page cache size" = 32;
-
-        # update interval
-        "update every" = 15;
-      };
-      ml = {
-        # enable machine learning
-        "enabled" = "yes";
-      };
-    };
+	 package = pkgs.netdata.override {
+ 		withCloud = true;
+ 	 };
   };
 
   virtualisation.oci-containers.containers = {
