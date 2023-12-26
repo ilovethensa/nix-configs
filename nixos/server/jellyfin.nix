d5cb2a1d9af05dc2a584da814929c182ddd836fb
Linus Torvalds <me@theholytachanka.com>
2023-12-26 20:40:56 +0200

    please work

--- nixos/server/jellyfin.nix
+++ nixos/server/jellyfin.nix
@@ -1,19 +1,25 @@
 { pkgs, lib,config, ... }:
 {
-  # 1. enable vaapi on OS-level
-  nixpkgs.config.packageOverrides = pkgs: {
-    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
-  };
-  hardware.opengl = {
-    enable = true;
-    extraPackages = with pkgs; [
-      intel-media-driver
-      vaapiIntel
-      vaapiVdpau
-      libvdpau-va-gl
-      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
-    ];
-  };
+    # Enable vaapi on OS-level
+    nixpkgs.config.packageOverrides = pkgs: {
+      vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+    };
+    hardware.opengl = {
+      enable = true;
+      extraPackages = [
+        pkgs.intel-media-driver
+        pkgs.vaapiIntel
+        pkgs.vaapiVdpau
+        pkgs.libvdpau-va-gl
+      ];
+    };
+
+    # Allow jellyfin user to access the graphics card
+    users.users.${config.services.jellyfin.user}.extraGroups = ["video" "render"];
+
+    # Override default hardening measure from NixOS
+    systemd.services.jellyfin.serviceConfig.PrivateDevices = lib.mkForce false;
+    systemd.services.jellyfin.serviceConfig.DeviceAllow = lib.mkForce ["/dev/dri/renderD128"];
 
   # 2. do not forget to enable jellyfin
   services.jellyfin = {
