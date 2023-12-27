9ba78bc09536d9ccff04bcd1c74e6ea8e2f74986
Linus Torvalds <me@theholytachanka.com>
2023-12-27 13:03:42 +0200

    added filebrowser

--- nixos/server/services.nix
+++ nixos/server/services.nix
@@ -10,34 +10,18 @@
   # Enable docker socket for homepage docker integration
   virtualisation.podman.dockerSocket.enable = true;
 
-  # Samba
-services.samba = {
-  enable = true;
-  securityType = "user";
-  extraConfig = ''
-    workgroup = WORKGROUP
-    server string = smbnix
-    netbios name = smbnix
-    security = user 
-    #use sendfile = yes
-    #max protocol = smb2
-    # note: localhost is the ipv6 localhost ::1
-    hosts allow = 192.168.0. 127.0.0.1 localhost
-    hosts deny = 0.0.0.0/0
-    guest account = nobody
-    map to guest = bad user
-  '';
-  shares = {
-    public = {
-      path = "/srv";
-      browseable = "yes";
-      "read only" = "no";
-      "guest ok" = "yes";
-    };
-  };
-};
-
   virtualisation.oci-containers.containers = {
+    filebrowser = {
+      image = "docker://filebrowser/filebrowser";
+      volumes = [
+        "/srv:/srv"
+        "/srv/AppData/filebrowser/config/database.db:/database.db"
+      ];
+      ports = [ 
+        "1010:80" 
+      ];
+      autoStart = true;
+    };
     bazarr = {
       image = "docker://lscr.io/linuxserver/bazarr:latest";
       volumes = [
