595b5e57ab47513ecdc9851281315e2a3eaa2550
Linus Torvalds <me@theholytachanka.com>
2024-01-06 11:16:38 +0000

    why dont you work

--- nixos/desktop/configuration.nix
+++ nixos/desktop/configuration.nix
@@ -229,7 +229,7 @@
     extraSpecialArgs = { inherit inputs outputs; };
     users = {
       # Import your home-manager configuration
-      tht = import ../home-manager/home.nix;
+      tht = import ../../home-manager/home.nix;
     };
   };
 
