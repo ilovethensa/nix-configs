219ddce145a8b6af56fa0cf1b52a0e8dcb2117cd
Linus Torvalds <me@theholytachanka.com>
2024-01-06 22:41:48 +0000

    added endlessh

--- nixos/common.nix
+++ nixos/common.nix
@@ -63,7 +63,9 @@
 | disclosed to the appropriate authorities.          |
 +----------------------------------------------------+
     '';
+    ports = "584"
   };
+  services.endlessh.enable = true;
   services.tailscale.enable = true;
   
 }
