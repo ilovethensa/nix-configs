07e385bb6422657e52f5fa7ed665062754b47c2a
Linus Torvalds <me@theholytachanka.com>
2024-01-06 23:02:09 +0000

    added autoupdates

--- nixos/common.nix
+++ nixos/common.nix
@@ -70,5 +70,18 @@
     port = 22;
   };
   services.tailscale.enable = true;
+
+  system.autoUpgrade = {
+    enable = true;
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" # print build logs
+    ];
+    dates = "02:00";
+    randomizedDelaySec = "45min";
+  };
+
   
 }
