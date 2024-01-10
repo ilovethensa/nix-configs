f298d3c913a367cf709c2fd3ead0eec4612556cb
Linus Torvalds <me@theholytachanka.com>
2024-01-10 16:05:48 +0000

    work pls

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
   };
 
   outputs =
-    { self, nixpkgs, home-manager, firefox-addons, impermanence, ... }@inputs:
+    { self, nixpkgs, home-manager, firefox-addons, impermanence, spicetify-nix, ... }@inputs:
     let
       inherit (self) outputs;
       # Supported systems for your flake packages, shell, etc.
