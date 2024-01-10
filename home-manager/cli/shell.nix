228d106b2c43c0692aa939b220a74071e1274d65
Linus Torvalds <me@theholytachanka.com>
2024-01-10 14:10:17 +0000

    removed zoxide

--- home-manager/cli/shell.nix
+++ home-manager/cli/shell.nix
@@ -3,19 +3,16 @@
   programs.fish = {
     interactiveShellInit = ''
     starship init fish | source
-    zoxide init fish | source
     '';
     enable = true;
     shellAliases = {
       ls = "exa";
-      cd = "z";
       cat = "bat";
     };
   };
   programs.thefuck.enable = true;
   programs.starship.enable = true;
   home.packages = with pkg; [
-      zoxide
       eza
       fuc
       bat
