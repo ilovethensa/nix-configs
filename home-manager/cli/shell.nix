f0a029b21e3053b49e4477f2a372c6ce15e6b1c1
Linus Torvalds <me@theholytachanka.com>
2024-01-11 13:23:21 +0000

    added function

--- home-manager/cli/shell.nix
+++ home-manager/cli/shell.nix
@@ -3,13 +3,21 @@
     fish = {
       interactiveShellInit = ''
         starship init fish | source
+function nix-init
+    if test -z $argv
+        echo "Usage: nix-init <ARGUMENT>"
+    else
+        nix flake init -t ~/nixos-configs#$argv
+    end
+end
+
       '';
       enable = true;
       shellAliases = {
         ls = "exa";
         cat = "bat";
-        nix-init = "nix flake init -t"
       };
+      
     };
     thefuck.enable = true;
     starship.enable = true;
