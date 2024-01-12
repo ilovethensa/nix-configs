a1c7072272e2b027e5db5cc3ff4abf074553337d
Linus Torvalds <me@theholytachanka.com>
2024-01-12 12:35:50 +0000

    added some extensions

--- home-manager/gui/firefox.nix
+++ home-manager/gui/firefox.nix
@@ -6,21 +6,23 @@
       id = 0;
       name = "Default";
       isDefault = true;
-      extensions = [
-        inputs.firefox-addons.packages."x86_64-linux".darkreader
-        inputs.firefox-addons.packages."x86_64-linux".ublock-origin
-        inputs.firefox-addons.packages."x86_64-linux".libredirect
-        inputs.firefox-addons.packages."x86_64-linux".sponsorblock
-        inputs.firefox-addons.packages."x86_64-linux".skip-redirect
-        inputs.firefox-addons.packages."x86_64-linux".lovely-forks
-        inputs.firefox-addons.packages."x86_64-linux".localcdn
-        inputs.firefox-addons.packages."x86_64-linux".istilldontcareaboutcookies
-        inputs.firefox-addons.packages."x86_64-linux".indie-wiki-buddy
-        inputs.firefox-addons.packages."x86_64-linux".dearrow
-        inputs.firefox-addons.packages."x86_64-linux".bulgarian-dictionary
-        inputs.firefox-addons.packages."x86_64-linux".boring-rss
-        inputs.firefox-addons.packages."x86_64-linux".bitwarden
-        inputs.firefox-addons.packages."x86_64-linux".augmented-steam
+      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+        darkreader
+        ublock-origin
+        libredirect
+        sponsorblock
+        skip-redirect
+        lovely-forks
+        localcdn
+        istilldontcareaboutcookies
+        indie-wiki-buddy
+        dearrow
+        bulgarian-dictionary
+        boring-rss
+        bitwarden
+        augmented-steam
+        omnivore
+        enhanced-github
       ];
       settings = {
 "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
