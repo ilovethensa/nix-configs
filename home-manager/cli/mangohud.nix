f2a5687712cea494df667fc3eeef0ef3c8162581
Linus Torvalds <me@theholytachanka.com>
2024-01-12 18:51:44 +0000

    fard

--- home-manager/cli/mangohud.nix
+++ home-manager/cli/mangohud.nix
@@ -1,26 +1,45 @@
-{ pkgs, lib, inputs, ... }: {
+{ pkgs, lib, inputs, ... }:
+
+{
   programs.mangohud = {
     enable = true;
     settings = {
-      fps_limit = "60,0";
-      vsync = 1;
-      cpu_stats = true;
-      cpu_temp = true;
+      toggle_fps_limit = "F1";
+      legacy_layout = false;
       gpu_stats = true;
       gpu_temp = true;
-      vulkan_driver = false;
+      gpu_load_change = true;  # Fix here
+      gpu_load_value = [50 90];  # Fix here
+      gpu_load_color = ["FFFFFF" "FF7800" "CC0000"];  # Fix here
+      gpu_text = "GPU";
+      cpu_stats = true;
+      cpu_temp = true;
+      cpu_load_change = true;  # Fix here
+      core_load_change = true;  # Fix here
+      cpu_load_value = [50 90];  # Fix here
+      cpu_load_color = ["FFFFFF" "FF7800" "CC0000"];  # Fix here
+      cpu_text = "CPU";
+      io_color = "a491d3";
+      vram = true;
+      vram_color = "ad64c1";
+      ram = true;
+      ram_color = "c26693";
       fps = true;
-      frametime = true;
-      frame_timing = true;
-      enableSessionWide = true;
+      engine_color = "eb5b5b";
+      wine_color = "eb5b5b";
+      frame_timing = 1;
+      media_player_color = "ffffff";
+      background_alpha = 0.4;
       font_size = 24;
+      background_color = "020202";
       position = "top-left";
-      engine_version = true;
-      wine = true;
-      no_display = true;
-      background_alpha = "0.5";
+      text_color = "ffffff";
+      round_corners = 0;
       toggle_hud = "Shift_R+F12";
-      toggle_fps_limit = "Shift_R+F1";
+      toggle_logging = "Shift_L+F2";
+      upload_log = "F5";
+      output_folder = "/home/tht";
+      media_player_name = "spotify";
     };
   };
 }
