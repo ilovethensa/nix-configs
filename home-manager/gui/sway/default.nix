0a5a5616a8a2fc85b7261061e184a685ee5da71d
Linus Torvalds <me@theholytachanka.com>
2024-01-28 15:27:57 +0000

    fix

--- home-manager/gui/sway/default.nix
+++ home-manager/gui/sway/default.nix
@@ -45,19 +45,6 @@ in
           outer = 4;
         };
 
-        startup = [
-          { command = "--no-startup-id ${pkgs.alsa-utils}/bin/amixer set Master 35%"; always = false; }
-          { command = "--no-startup-id ${pkgs.redshift}/bin/redshift -l 50.77083:3.57361 -t 6500K:3000K"; always = false; }
-        ] ++ lib.optionals (displayServer == "wayland") [
-          # Wallpaper
-          { command = "--no-startup-id ${pkgs.swaybg}/bin/swaybg -i ${config.xdg.configHome}/wallpapers/spirited_away.png"; always = false; }
-        ] ++ lib.optionals (displayServer == "xsession") [
-          # Wallpaper
-          { command = "--no-startup-id ${pkgs.xwallpaper}/bin/xwallpaper --daemon --zoom ${config.xdg.configHome}/wallpapers/spirited_away.png"; always = false; }
-          # Tray icon application that lets you disable auto-sleep
-          { command = "--no-startup-id ${pkgs.caffeine-ng}/bin/caffeine"; always = false; }
-        ];
-
         assigns = {
           "${workspaces.ws2}" = [{ class = "electronplayer"; }];
           "${workspaces.ws3}" = [
@@ -75,12 +62,7 @@ in
           { class = "ghidra-Ghidra"; title = "^(?!(CodeBrowser.*|Ghidra.*))"; }
         ];
       };
-    } // lib.optionalAttrs (windowManager == "sway") {
       wrapperFeatures.gtk = true;
     };
-  } // lib.optionalAttrs (displayServer == "xsession") {
-    # Generate a script to start the X server, used by the login manager
-    enable = true;
-    scriptPath = ".hm-graphical-session";
   };
 }
