779e20f5a87e45dce9531c1fec5f0a53fbd38d2a
Linus Torvalds <me@theholytachanka.com>
2024-01-29 22:11:46 +0000

    stuff

--- home-manager/cli/scripts/movie-play.nix
+++ home-manager/cli/scripts/movie-play.nix
@@ -1,4 +1,4 @@
-{ pkgs }:
+{ pkgs, lib, inputs, ... }:
 
 pkgs.writeShellScriptBin "movie" ''
     foot -a launcher command bash -c "${pkgs.findutils}/bin/find ~/Downloads -type f -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.avi" | ${pkgs.fzf}/bin/fzf | xargs ${pkgs.vlc}/bin/vlc"
