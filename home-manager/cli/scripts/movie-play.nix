4c3cbcedf94dafba47ec554bbb3171edf18e77da
Linus Torvalds <me@theholytachanka.com>
2024-01-29 22:09:35 +0000

    updated movie script

--- home-manager/cli/scripts/movie-play.nix
+++ home-manager/cli/scripts/movie-play.nix
@@ -1,5 +1,5 @@
 { pkgs }:
 
 pkgs.writeShellScriptBin "movie" ''
-    ${pkgs.findutils}/bin/find ~/Downloads -type f -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.avi" -exec ${pkgs.vlc}/bin/vlc {} +
+    ${pkgs.findutils}/bin/find ~/Downloads -type f -iname "*.mp4" -o -iname "*.mkv" -o -iname "*.avi" | ${pkgs.fzf}/bin/fzf | xargs ${pkgs.vlc}/bin/vlc
 ''
