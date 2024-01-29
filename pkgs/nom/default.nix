bac05b1e34d692279a73a05fe6def4704ec91dbf
Linus Torvalds <me@theholytachanka.com>
2024-01-29 19:00:03 +0000

    added nom

--- pkgs/nom/default.nix
+++ pkgs/nom/default.nix
@@ -1,8 +1,4 @@
-{ pkgs ? import (fetchTarball {
-  url =
-    "https://github.com/NixOS/nixpkgs/archive/4fe8d07066f6ea82cda2b0c9ae7aee59b2d241b3.tar.gz";
-  sha256 = "sha256:06jzngg5jm1f81sc4xfskvvgjy5bblz51xpl788mnps1wrkykfhp";
-}) { } }:
+{ fetchFromGitHub, buildGoModule, lib, pkg, ... }:
 buildGoModule rec {
   pname = "nom";
   version = "2.1.1";
@@ -14,7 +10,7 @@ buildGoModule rec {
     sha256 = "0m2fzpqxk7hrbxsgqplkg7h2p7gv6s1miymv3gvw0cz039skag0s";
   };
 
-  vendorSha256 = "1879j77k96684wi554rkjxydrj8g3hpp0kvxz03sd8dmwr3lh83j";
+  vendorHash = "sha256-6hCgv2/8UIRHw1kCe3nLkxF23zE/7t5RDwEjSzX3pBQ=";
 
   meta = with lib; {
     description = "RSS reader for the terminal ";
